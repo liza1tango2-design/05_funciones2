08741afb09ea4e325bd69bc31d47d6782e183b3e
liza1tango2-design <liza1tango2@gmail.com>
2025-10-15 15:53:21 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+//ejercicios del práctico 06 de Funciones 02
+console.log("================================");
+console.log(" Práctico 05 - Funciones II");
+console.log("================================");
+
+//1.Función nativa de JavaScript
+function saludar(){
+    console.log("Buenas tardes, soy la funciónsaludar");
+}
+//para ejecutar la funcion debemos llmarla
+saludar()
+
+// Funciones pata el login de el usuario
+function Login(){
+    //1.captura de datos del formulario
+}
